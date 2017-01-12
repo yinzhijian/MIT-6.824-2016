34475548a7243a4bdcc6e382e84ad9d161c8f84f
yintao <yintao000@gmail.com>
2017-01-12 14:13:45 +0000

    update schedule.go

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -14,7 +17,6 @@ func (mr *Master) schedule(phase jobPhase) {
 		ntasks = mr.nReduce
 		nios = len(mr.files)
 	}
-
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
 	// All ntasks tasks have to be scheduled on workers, and only once all of
@@ -24,5 +26,24 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	for ntasks > 0{
+		w := <-mr.registerChannel
+		go func(w string,ntasks *int){
+			*ntasks = *ntasks - 1
+			args := new(DoTaskArgs)
+			args.JobName = "task"+strconv.Itoa(*ntasks)
+			args.Phase = phase
+			args.TaskNumber = *ntasks
+			args.NumOtherPhase = nios
+			args.File = mr.files[*ntasks]
+			ok := call(w, "Worker.DoTask", args, new(struct{}))
+			if ok {
+				mr.registerChannel <- w
+			}else{
+				*ntasks = *ntasks + 1
+			}
+		}(w,&ntasks)
+		//worker := <-mr.registerChannel
+	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
